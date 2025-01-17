7dfca3f644e3f886228628dc37dfa1b83a4232d0
raiza-de-paula <raiza.paula@ab-inbev.com>
2025-01-17 04:25:06 -0300

    [validation-iti-service] Open Api

--- src/test/kotlin/br/com/validator/iti/model/ValidatorFactoryTest.kt
+++ src/test/kotlin/br/com/validator/iti/model/ValidatorFactoryTest.kt
@@ -2,9 +2,12 @@ package br.com.validator.iti.model
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@ExtendWith(SpringExtension::class)
 @SpringBootTest
 class ValidatorFactoryTest {
 
